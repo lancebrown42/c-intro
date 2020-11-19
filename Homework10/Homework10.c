70939f0e9dbdc1081eb0f0dafcc3e05183b5ac7e
Lance <lance.brown42@gmail.com>
2020-11-19 16:47:04 -0500

    finished 10

--- Homework10/Homework10.c
+++ Homework10/Homework10.c
@@ -1,7 +1,7 @@
 // ------------------------------------------------------------------------------------------
 // Name: Lance Brown
 // Class: C Programming
-// Abstract: Homework 
+// Abstract: Homework 10
 // ------------------------------------------------------------------------------------------
 
 // ------------------------------------------------------------------------------------------
@@ -10,6 +10,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 // ------------------------------------------------------------------------------------------
 // Constants
@@ -19,6 +20,15 @@
 // Prototypes
 // ------------------------------------------------------------------------------------------
 int ValidateInput();
+int AllocateArray(int *arrArray);
+void InitializeArray(int *arrArray, int intArrSize);
+void PopulateArray(int *arrArray, int intArrSize);
+void PrintArray(int *arrArray, int intArrSize);
+int ArrayMax(int *arrArray, int intArrSize);
+int ArrayMin(int *arrArray, int intArrSize);
+int ArrayTotal(int *arrArray, int intArrSize);
+double ArrayAverage(int *arrArray, int intArrSize);
+double ArrayStdDev(int *arrArray, int intArrSize);
 // ------------------------------------------------------------------------------------------
 // Name: main
 // Abstract: This is where the program starts
@@ -26,21 +36,26 @@ int ValidateInput();
 void main()
 {
 	int intArrSize = 0;
-	int arrArray[] = 0;
 	int intIndex = 0;
-	int intInput = 0;
-	printf("Enter an integer to size the array:\n");
-	intArrSize = ValidateInput();
-	malloc(arrArray, intArrSize * sizeof(int));
-	while (intIndex < intArrSize) {
-		arrArray[intIndex] = 0;
-		intIndex++;
-	}
-	intIndex = 0;
-	while (intIndex < intArrSize) {
-		printf("Enter integer %d: ", intIndex + 1);
-		arrArray[intIndex] = ValidateInput();
-	}
+	unsigned int intInput = 0;
+	int* arrArray = malloc(sizeof(int));
+	//1
+	intArrSize = AllocateArray(arrArray);
+	//2
+	InitializeArray(arrArray, intArrSize);
+	//3
+	PopulateArray(arrArray, intArrSize);
+	//4
+	PrintArray(arrArray, intArrSize);
+	//5	
+	printf("The max is %d\n", ArrayMax(arrArray, intArrSize));
+	//6
+	printf("The min is %d\n", ArrayMin(arrArray, intArrSize));
+	//7
+	printf("The total is %d\n", ArrayTotal(arrArray, intArrSize));
+	//8
+	printf("The standard deviation is %f\n", ArrayStdDev(arrArray, intArrSize));
+	system("pause");
 	
 }
 
@@ -64,4 +79,136 @@ int ValidateInput() {
 		intReturn = ValidateInput();
 	}
 	return intReturn;
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: AllocateArray
+// Abstract: allocates array
+// ------------------------------------------------------------------------------------------
+int AllocateArray(int *arrArray) {
+	int intArrSize = 0;
+	printf("Enter an integer to size the array:\n");
+	intArrSize = ValidateInput();
+	arrArray = malloc(intArrSize * sizeof(int));
+	return intArrSize;
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: InitializeArray
+// Abstract: initializes array
+// ------------------------------------------------------------------------------------------
+void InitializeArray(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	while (intIndex < intArrSize) {
+		arrArray[intIndex] = 0;
+		intIndex++;
+	}
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: PopulateArray
+// Abstract: populates array from user input
+// ------------------------------------------------------------------------------------------
+void PopulateArray(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	while (intIndex < intArrSize) {
+		printf("Enter integer %d: ", intIndex + 1);
+		arrArray[intIndex] = ValidateInput();
+		intIndex++;
+	}
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: PrintArray
+// Abstract: prints full array
+// ------------------------------------------------------------------------------------------
+void PrintArray(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	printf("Array Contents:\n---------------------------------------------------------------------\n");
+	while (intIndex < intArrSize) {
+		printf("Location[%d] = %d\n", intIndex + 1, arrArray[intIndex]);
+		intIndex++;
+	}
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: ArrayMax
+// Abstract: Finds maximum value
+// ------------------------------------------------------------------------------------------
+int ArrayMax(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	int intMax = 0;
+	while (intIndex < intArrSize) {
+		if (arrArray[intIndex] > intMax) {
+			intMax = arrArray[intIndex];
+		}
+		intIndex++;
+	}
+	return intMax;
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: ArrayMin
+// Abstract: Finds minimum value
+// ------------------------------------------------------------------------------------------
+int ArrayMin(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	int intMin = arrArray[intIndex];
+	while (intIndex < intArrSize) {
+		if (arrArray[intIndex] < intMin) {
+			intMin = arrArray[intIndex];
+		}
+		intIndex++;
+	}
+	return intMin;
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: ArrayTotal
+// Abstract: sums array
+// ------------------------------------------------------------------------------------------
+int ArrayTotal(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	int intTotal = 0;
+	while (intIndex < intArrSize) {
+		intTotal += arrArray[intIndex];
+		intIndex++;
+	}
+	return intTotal;
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: ArrayAverage
+// Abstract: finds mean
+// ------------------------------------------------------------------------------------------
+double ArrayAverage(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	double dblAverage = 0;
+	dblAverage = ArrayTotal(arrArray, intArrSize) / intArrSize;
+	return dblAverage;
+}
+
+
+// ------------------------------------------------------------------------------------------
+// Name: ArrayStdDev
+// Abstract: finds standard deviation
+// ------------------------------------------------------------------------------------------
+double ArrayStdDev(int *arrArray, int intArrSize) {
+	int intIndex = 0;
+	double dblAvg = ArrayAverage(arrArray, intArrSize);
+	double dblStdDev = 0;
+	while (intIndex < intArrSize){
+		dblStdDev += pow(arrArray[intIndex] - dblAvg, 2);
+		intIndex++;
+	}
+	dblStdDev = dblStdDev / intArrSize;
+	return sqrt(dblStdDev);
 }
